85cf7f8aa9e07c76bca545071125a82c9a80373d
Sriram <73679247+sriramshiyam@users.noreply.github.com>
2021-10-27 03:19:54 -1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/about" component={About}  />
-          <Route path="/contact" component={Contact}  />
+          <Route path="/about" component={About} exact />
+          <Route path="/contact" component={Contact} exact />
           <Route component={NotFound} />
         </Switch>
         <Footer />
